d4239e8b6dd7e03edb94afc279307add39df2716
Bharath200415 <bharath200415@gmail.com>
2025-07-15 00:38:58 +0530

    added CORS

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/sum",function(req,res){
     const a = req.query.a;
